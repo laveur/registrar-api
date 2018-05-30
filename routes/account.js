1a650c93f168b423c9a234fa2707ba4d07f80161
laveur <ntbourey@me.com>
2018-05-29 17:50:47 -0700

    Implement Account API
    Implmented Account basics

--- routes/account.js
+++ routes/account.js
@@ -1,24 +1,72 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
+var util = require('util');
+
+router.get('/', function(req, res, next) {
+    // console.log(util.inspect(db));
+    db.Accounts.findAll().then(accounts => {
+        results = [];
+        accounts.forEach(function(account) {
+            result = account.toJSON();
+
+            results.push(result);
+        });
+
+        res.json({accounts: results});
+    });
+});
 
 router.post('/', function(req, res, next) {
-    res.status(201);
-    res.send('Created');
+    db.Accounts.create(req.body).then(account => {
+        res.status(201);
+        res.set('Location', '/account/' + account.id);
+        res.json({message: 'Created'});
+    }).catch(db.Sequelize.ValidationError, function (error) {
+        if (error.name == "SequelizeUniqueConstraintError") {
+            res.status(409);
+            res.json({message: 'Conflict: Account exists'});
+        } else {
+            res.status(400);
+            res.json({message: 'Bad Request'});
+        }
+    });
 });
 
 router.get('/:id', function(req, res, next) {
-    res.status(404);
-    res.send('Not Found');
+    db.Accounts.findById(req.params.id).then(account => {
+        if (account != null) {
+            res.json(account);
+        } else {
+            res.status(404);
+            res.json({message: 'Account Not Found'});
+        }
+    });
 });
 
 router.put('/:id', function(req, res, next) {
-    res.status(404);
-    res.send('Not Found');
+    db.Accounts.findById(req.params.id).then(account => {
+        if (account != null) {
+            // TODO: Update account
+            res.json(account.get());
+        } else {
+            res.status(404);
+            res.json({message: 'Account Not Found'});
+        }
+    });
 });
 
 router.delete('/:id', function(req, res, next) {
-    res.status(404);
-    res.send('Not Found');
+    db.Accounts.findById(req.params.id).then(account => {
+        if (account != null) {
+            return account.destroy();
+        } else {
+            res.status(404);
+            res.json({message: 'Account Not Found'});
+        }
+    }).then(() => {
+        res.json({message: 'Account deleted'});
+    });
 });
 
 module.exports = router;
